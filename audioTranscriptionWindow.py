3a872b1b6d5cc77620be16d378d7a025020557b9
James-C-000 <James.Caldwell.000@gmail.com>
2025-03-30 17:54:20 -0400

    Cleaned up old audio transcription stuff

--- audioTranscriptionWindow.py
+++ audioTranscriptionWindow.py
@@ -1,10 +1,12 @@
+# This script takes a directory of audio files and creates a text transcription of each file, output to the provided
+# directory. Uses OpenAI Whisper.
+# Note: latest PyPI openai-whisper package is broken with python >3.12. If you are building this locally, set up a local
+# venv package with the git version (see https://github.com/openai/whisper/discussions/2410)
+
 #!/usr/bin/python3
 import os
-import shutil
-import time
 import tkinter as tk
 from tkinter import messagebox
-import ocrmypdf
 import pygubu
 import threading
 import whisper
